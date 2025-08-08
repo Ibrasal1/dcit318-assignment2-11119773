19e026aafee3ec5849caf8fbaa69bc17323d1b27
IBRAHIM BABA SALIFU <ibrasal360@gmail.com>
2025-08-08 12:16:43 +0000

    Added InterfaceDemo project

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace InterfaceDemo
+{
+    // Interface
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Car class
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Bicycle class
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INTERFACE DEMO\n");
+
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();       // Output: Car is moving
+            bicycle.Move();   // Output: Bicycle is moving
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
